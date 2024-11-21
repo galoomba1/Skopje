6202a9bf401339ad708b98c7f82217b3412306d8
galoomba1 <69735183+galoomba1@users.noreply.github.com>
2024-11-21 16:30:42 +0100

    Create b3-cejs234eijkrw5cry6ik7c.py

---
+++ skopje/b3-cejs234eijkrw5cry6ik7c.py
@@ -0,0 +1,17 @@
+import lifelib
+sess = lifelib.load_rules("b3-cejs234eijkrw5cry6ik7c")
+lt = sess.lifetree(n_layers=1)
+
+fixeds = """1 xs4_33 4 block 
+1 xs4_252 4 tub
+2 xp2_7 3 blinker
+3 xp3_cihhe2z01 13
+4 xp4_3546mo 11
+5 xp5_3eqfp 15
+6 xp6_354493 10
+7 xp7_kv2rhlr23zx66 27
+8 xp8_mkmg53z1 14
+9 xp9_25is 8
+10 xp10_0gc2a52z653 15
+"""
+cfuncs = ()
